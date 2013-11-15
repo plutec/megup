37375503e98f34b4f6a2dadcc0cdef2cf98e9754
asanchez <asanchez@plutec.net>
2013-11-15 17:57:47 +0100

    Adding parser options

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import backup
 import settings
-import uploader
-import filesystem
+import argparse
 
+def parsing_args():
+    parser = argparse.ArgumentParser(
+                                description='Make a backup in Mega service.')
+    parser.add_argument('-s', metavar='FILE', type=str, nargs=1,
+                       help='backup origin folder', dest='syncfile')
+    parser.add_argument('-v', dest='verbose', metavar="X", type=int, default=5,
+                       help='verbose level number')
+
+    args = parser.parse_args()
+    #print args
+    return args
 
 def main():
-	b = backup.Backup(settings.settings['sync_file'])
-	#b.is_initial_backup()
-	b.run()
+    b = backup.Backup(settings.settings['sync_file'])
+    #b.is_initial_backup()
+    b.run()
 
 if __name__ == '__main__':
-	main()
+    options = parsing_args()
+    main()
