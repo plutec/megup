325d2b20d262c2ce3f8cf629985ed9906753d6ab
Alberto Aguilera <drimer.aaj@gmail.com>
2013-11-28 23:54:10 +0000

    tests/run_test_suite.sh: Add working infrastructure for end-to-end tests
    The only existing test we've got at the moment is a unit test that can be
    run stand-alone as it doesn't depend on any external source. However,
    because this branch is going to change some code so that it uses a
    configuration file in two different ways, we'll need to start using an
    end-to-end test to prove that our changes actually work.
    
    Now, these tests will be comprised by bash script files with a very
    specific structure:
    
    1) Every file that contains end-to-end tests must have a name in the form
       "test-*.sh"
    
    2) The tests in these files will be defined as functions with names in the
       form "test_*"
    
    Therefore, if helper functions that don't meet those requirements are
    declared, they won't be considered as tests.

--- tests/run_test_suite.sh
+++ tests/run_test_suite.sh
@@ -4,3 +4,26 @@
 # when they want to run the test suite.
 
 nosetests -sv .
+
+
+# Import end-to-end scripts
+own_dir="$(dirname "$0")"
+for f in $own_dir/end_to_end/test-*.sh; do
+    source $f
+done
+
+
+# Run all existing end-to-end tests
+echo "-- END-TO-END Tests Results --"
+cd $own_dir/..  # Necessary for tests to have immediate access to main.py
+
+ret=0
+for t in $(declare -F | awk '/ test_/ {print $3}'); do
+    echo -n "$t ... "
+    $t >/dev/null 2>&1
+    [[ $? == 0 ]] && echo "PASSED" || {
+	echo "FAILED";
+        ret=1;
+    }
+done
+exit $ret
