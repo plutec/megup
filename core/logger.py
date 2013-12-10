def6981c0473a87d0a7b418c473834ac72d40c2b
asanchez <asanchez@plutec.net>
2013-12-10 12:29:27 +0100

    #14 Changed logged

--- core/logger.py
+++ core/logger.py
@@ -7,43 +7,41 @@ import logging.handlers
 import os
 
 log = logging.getLogger('megup')
-log.setLevel(logging.DEBUG)
 
-# create file handlers
-dh = logging.FileHandler(os.path.join(
-						settings.get_config('global','logs_folder'), 'debug'))
-dh.setLevel(logging.DEBUG)
+levels = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
+          logging.CRITICAL]
 
-ih = logging.FileHandler(os.path.join(
-						settings.get_config('global','logs_folder'), 'info'))
-ih.setLevel(logging.INFO)
+try:
+    log_level = int(settings.get_config('global', 'log_level'))
+except:
+    log_level = 5
 
-wh = logging.FileHandler(os.path.join(
-						settings.get_config('global','logs_folder'), 'warning'))
-wh.setLevel(logging.WARNING)
+# Establising limits
+if log_level < 0:
+    log_level = 0
+elif log_level >= len(levels):
+    log_level = len(levels) - 1
 
-eh = logging.FileHandler(os.path.join(
-						settings.get_config('global','logs_folder'),'error'))
-eh.setLevel(logging.ERROR)
 
-ch = logging.FileHandler(os.path.join(
-						settings.get_config('global','logs_folder'),'critical'))
-ch.setLevel(logging.CRITICAL)
+log.setLevel(levels[log_level])
+
+file_handler = logging.FileHandler(os.path.join(
+                        settings.get_config('global', 'logs_folder'), 'log'))
+file_handler.setLevel(levels[log_level])
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(levels[log_level])
 
 # create formatter and add it to the handlers
 formatter = logging.Formatter( \
 '[%(levelname)s] %(asctime)s - %(funcName)s@%(filename)s: %(message)s' \
                                 , datefmt='%Y/%m/%d %H:%M:%S')
 
-dh.setFormatter(formatter)
-ih.setFormatter(formatter)
-wh.setFormatter(formatter)
-eh.setFormatter(formatter)
-ch.setFormatter(formatter)
+
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
 
 # add the handlers to the logger
-log.addHandler(dh)
-log.addHandler(ih)
-log.addHandler(wh)
-log.addHandler(eh)
-log.addHandler(ch)
+
+log.addHandler(file_handler)
+log.addHandler(stream_handler)
